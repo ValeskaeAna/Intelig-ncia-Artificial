759bec9e4d0067c3c0c31cc66ee8a9a083e622ec
ValeskaeAna <159259271+ValeskaeAna@users.noreply.github.com>
2024-05-24 10:47:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,42 @@ const caixaPrincipal = document.querySelector(".caixa-pricipal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios hiper-realistas. Qual o primeiro pensamento?"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+];
